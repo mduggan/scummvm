d197345e0ab605f2a728a7ec20e53308d650893a
Matthew Duggan <mgithub@guarana.org>
2025-07-27 11:39:02 +1000

    ACCESS: Rename game enum vals to match ScummVM style

--- engines/access/screen.cpp
+++ engines/access/screen.cpp
@@ -125,7 +125,7 @@ void Screen::setManPalette() {
 }
 
 void Screen::setIconPalette() {
-	if (_vm->getGameID() == GType_MartianMemorandum) {
+	if (_vm->getGameID() == kGameMartianMemorandum) {
 		for (int i = 0; i < 0x1B; i++) {
 			_rawPalette[741 + i] = VGA_COLOR_TRANS(Martian::ICON_PALETTE[i]);
 		}
